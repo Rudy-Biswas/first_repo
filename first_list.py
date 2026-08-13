ff49615dd2b79a2d55c56adecc128949e667ebde
Rudy-Biswas <rudrabiswas2010b@gmail.com>
2026-08-13 21:08:45 +0530

    first assignment list

---
+++ first_list.py
@@ -0,0 +1,13 @@
+w = input("Enter the first name: ")
+x = input("Enter the first name: ")
+y = input("Enter the first name: ")
+z = input("Enter the first name: ")
+
+items_list = [w,x,y,z]
+
+print(items_list[0] , items_list[1])
+print(items_list[1] , items_list[2])
+print(items_list[0:4])
+print(items_list[1:4])
+
+
