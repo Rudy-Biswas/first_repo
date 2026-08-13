d700fce9e2e8eb416af3e87292a61cb90d9d3167
Rudy-Biswas <rudrabiswas2010b@gmail.com>
2026-08-13 20:38:04 +0530

    first assignment

---
+++ first_assignment.py
@@ -0,0 +1,12 @@
+w = int(input("Enter the value of w: "))
+x = int(input("Enter the value of x: "))
+y = int(input("Enter the value of y: "))
+z = int(input("Enter the value of z: "))
+
+if w>x and y<z:
+    print("Hurray!!")
+elif w==y and x==z:
+    print("Hello..")
+else :
+    print("Fail")
+    
