eed945bbd69eabdaaf935f40652f7ccaaf85cc85
Rudy-Biswas <rudrabiswas2010b@gmail.com>
2026-08-16 21:23:58 +0530

    question_2

---
+++ question_2.py
@@ -0,0 +1,13 @@
+a = int(input("Enter your marks of Bengali: "))
+
+if a >= 90 and a <= 100 :
+    print("Grade-A")
+elif a >= 80 and a < 90 :
+    print("Grade-B")
+elif a >= 70 and a < 80 :
+    print("Grade-C")
+elif a >= 60 and a < 70 :
+    print("Grade-D")
+else :
+    print("You have failed the exam mf")
+    
