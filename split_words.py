19c230966a33eb3303a9f8ea7da968cd265424fc
Rudy-Biswas <rudrabiswas2010b@gmail.com>
2026-08-30 21:14:53 +0530

    split-words

---
+++ split_words.py
@@ -0,0 +1,13 @@
+sentence = input("Write the sentence: ")
+new_list = []
+word_list = sentence.split()
+
+for word in word_list:
+   sum(len(word))/len(word_list)
+   new_list.append(word)
+
+    
+      
+
+print(new_list)
+
